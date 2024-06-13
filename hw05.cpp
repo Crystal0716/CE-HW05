3478d65de3d900a201307a1f28006c306b74aa6d
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-13 21:05:01 +0800

    Add files via upload

---
+++ hw05.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <stdio.h>
+using namespace std ;
+int main ()
+{
+	int x = 129 ;
+	char c ;
+	c = x ;
+	int y ;
+	y = c ;
+	cout << " x = " << x << " y = " << y ;
+
+	return (0);
+}
